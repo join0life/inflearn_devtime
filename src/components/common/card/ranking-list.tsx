b0e7986542b1238dfa1c7a5f99e60c8665233223
Inyoung Cho <cupshop123@gmail.com>
2026-01-30 23:32:42 +0900

    docs: PULL_REQUEST_TEMPLATE.md 수정

--- src/components/common/card/ranking-list.tsx
+++ src/components/common/card/ranking-list.tsx
@@ -4,6 +4,7 @@ import RankingItem from "./ranking-item";
 const RankingList = () => {
   return (
     <div className="flex-col-center gap-3">
+      {/** @TODO mock 데이터 삭제 후 데이터 페칭 */}
       {mockData.map((mock) =>
         mock.data.rankings.map((rank) => (
           <RankingItem key={rank.userId} {...rank} />
